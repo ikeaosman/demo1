e3d941acb161a5eeb83249b30efa871ae4705737
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-15 09:05:33 +0100

    2.commit fra intelliJ

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,5 +8,6 @@ public class HomeController {
     @GetMapping("/")
     public String index(){
         return "Hello Ikhra"; //Det jeg skriver kan jeg se på en hjemmeside hvis jeg skriver localhost 8080
+        //test ændring
     }
 }
